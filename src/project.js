0d91d094e768adea99168f855e2c6558e2fdc35b
Heber <116190313+hpagon@users.noreply.github.com>
2024-06-09 16:54:33 -0700

    Created todo and project classes

--- src/project.js
+++ src/project.js
@@ -1,3 +1,30 @@
 export default class Project {
-    
+    #title;
+    #items;
+    #view;
+    constructor(title) {
+        this.#title = title;
+        this.#items = [];
+        this.#view = "List";
+    }
+    //getters
+    getTitle() {
+        return this.#title;
+    }
+    getItems() {
+        return this.#items;
+    }
+    getView() {
+        return this.#view;
+    }
+    //setters
+    setTitle(newTitle) {
+        this.#title = newTitle;
+    }
+    setView(newView) {
+        this.#view = newView;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
 }
--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,55 @@
-export default class Todo {}
+export default class Todo {
+  #title;
+  #description;
+  #dueDate;
+  #priority;
+  #status;
+  #projects;
+  
+  constructor(title, description, dueDate, priority, status, projects) {
+    this.#title = title;
+    this.#description = description;
+    this.#dueDate = dueDate;
+    this.#priority = priority;
+    this.#status = status;
+    this.#projects = projects;
+  }
+  //getters
+  getTitle() {
+    return this.#title;
+  }
+  getDescription() {
+    return this.#description;
+  }
+  getDate() {
+    return this.#dueDate;
+  }
+  getPriority() {
+    return this.#priority;
+  }
+  getStatus() {
+    return this.#status;
+  }
+  getProjects() {
+    return this.#projects;
+  }
+  //setters
+  setTitle(newTitle) {
+    this.#title = newTitle;
+  }
+  setDescription(newDescription) {
+    this.#description = newDescription;
+  }
+  setDate(newDate) {
+    this.#dueDate = newDate;
+  }
+  setPriority(newPriority) {
+    this.#priority = newPriority;
+  }
+  setStatus(newStatus) {
+    this.#status = newStatus;
+  }
+  setProjects(newProjects) {
+    this.#projects = newProjects;
+  }
+}
