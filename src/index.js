ccb7c6fcb59109858435c6b7b32bc058b017e649
Heber <116190313+hpagon@users.noreply.github.com>
2024-06-09 16:10:50 -0700

    Added js files for structure and made html outline

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./style.css";
+import DomHandler from "./domHandler";
+import ScreenController from "./screenController";
+import Project from "./project";
+import Todo from "./todo";
 
-console.log("working");
+class App {}
